34f078de41a15df84c4891196f76e9eb335f5496
David Argoanto <argoantodavid@gmail.com>
2023-11-20 21:31:24 +0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+        <h1 aref ="./" >Home</h1>
+        <h2 aref= "./">Service</h2>
+        <h3>Others</h3>
+      </div>
+      
+    </>
+  )
+}
+
+export default App
